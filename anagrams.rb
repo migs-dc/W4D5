7919b86e0bc0ec26e6f876cacb99640ee8d06d2e
Miguel Dela Cruz <delacruzcms@gmail.com>
2021-05-14 17:20:26 -0400

    finished phase II of anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -1,11 +1,4 @@
-# Anagrams
-# anagram?("gizmo", "sally")    #=> false
-# anagram?("elvis", "lives")    #=> true
-
-def first_anagram?()
-
-end
-
+require "byebug"
 
 def permutations(arr)
   return arr if arr.length <= 1
@@ -20,3 +13,51 @@ def permutations(arr)
   end
   res
 end
+
+#phase 1
+
+def first_anagram?(str1, str2)
+
+  perms = permutations(str1.split("")) # n!
+  perms = perms.map { |ele| ele.join("") } #n
+  perms.include?(str2) #n
+
+end 
+
+#time complexity is n! + 2n
+
+# p (1..15).to_a.permutation.to_a.length
+
+# 2 - 2
+# 3 - 6
+# 4 - 24
+# 5 - 120
+# 6 - 720
+# 7 - 5,040
+#10 - 3,628,800
+#15 - 1,307,674,368,000
+#100 - 93,326,215,443,944,200,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000,000
+
+# p permutations([1, 2, 3, 4]).length # => [[1, 2, 3], [1, 3, 2],
+#                                    #     [2, 1, 3], [2, 3, 1],
+#                                    #     [3, 1, 2], [3, 2, 1]]
+
+
+#phase 2
+
+def second_anagram?(str1, str2)
+  # debugger
+  arr = str2.split("") #
+  str1.each_char do |char|
+    if arr.include?(char)
+      i = arr.index(char)    
+      arr.delete_at(i)
+    end
+  end
+  arr.join("") == ""
+end
+
+
+# Anagrams
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
